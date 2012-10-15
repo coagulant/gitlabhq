2f0c4db0fa60195471d5864324afb4d9385d1019
Ilya Baryshev <baryshev@gmail.com>
2012-10-16 00:04:58 +0400

    Wiki search

--- features/steps/dashboard/dashboard_search.rb
+++ features/steps/dashboard/dashboard_search.rb
@@ -7,6 +7,17 @@ class DashboardSearch < Spinach::FeatureSteps
     click_button "Search"
   end
 
+  # FIXME: refactor 1 method
+  Given 'I search for "Contibuting"' do
+    fill_in "dashboard_search", :with => "Contibuting"
+    click_button "Search"
+  end
+
+  # FIXME: refactor 1 method
+  Then 'I should see "Contibuting guide" wiki link' do
+    page.should have_link "Contibuting guide"
+  end
+
   Then 'I should see "Shop" project link' do
     page.should have_link "Shop"
   end
@@ -15,4 +26,11 @@ class DashboardSearch < Spinach::FeatureSteps
     @project = Factory :project, :name => "Shop"
     @project.add_access(@user, :admin)
   end
+
+  And 'Project "Shop" has wiki page "Contibuting guide"' do
+    @wiki_page = Factory :wiki, :project => @project,
+                         :title => "Contibuting guide",
+                         :slug => "contributing"
+  end
+
 end
